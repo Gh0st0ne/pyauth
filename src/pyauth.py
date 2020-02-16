f3cde66b6b23f01ecd874f55f5fbe6e812527966
makseli <makseli>
2020-02-16 23:58:32 +0300

    .gitignore ide file and close app message added :)

--- src/pyauth.py
+++ src/pyauth.py
@@ -7,15 +7,17 @@ from parsers import parseSSH, parseFTP, parsePostgres
 from threading import Thread
 import helpers
 
-#We are monitoring the given file contionusly in here.
+
+# We are monitoring the given file contionusly in here.
 def monitorTheFile(logFile):
-    logFile.seek(0,2)
+    logFile.seek(0, 2)
     while True:
         line = logFile.readline()
         if not line:
             time.sleep(0.1)
             continue
         yield line
+
 
 def analyzeLogs(logFileName, **type):
     with open(logFileName, 'r') as logFile:
@@ -35,27 +37,32 @@ def analyzeLogs(logFileName, **type):
             for line in logLines:
                 if re.findall(helpers.regexForPostgresFail, line):
                     logEntry, user = parsePostgres.parsePostgresLine(line)
-                    print(helpers.bcolors.FAIL + "[FAILED] " + helpers.bcolors.ENDC + "%s failed login attempt for %s" % (logEntry.strip(), user))
+                    print(
+                        helpers.bcolors.FAIL + "[FAILED] " + helpers.bcolors.ENDC + "%s failed login attempt for %s" % (
+                            logEntry.strip(), user))
 
 
 def isExist(fileName):
     if os.path.isfile(fileName):
         return True
 
+
 def startMonitoring(distribution):
     with open("fileList.yaml", 'r') as stream:
         try:
             yamlData = yaml.safe_load(stream)
             if isExist(yamlData[distribution]["auth"]):
-                Thread(target=analyzeLogs, args=(yamlData[distribution]["auth"],), kwargs={'key1':"SSH", 'key2':"FTP"}).start()
+                Thread(target=analyzeLogs, args=(yamlData[distribution]["auth"],),
+                       kwargs={'key1': "SSH", 'key2': "FTP"}).start()
             if isExist(yamlData[distribution]["postgres"]):
-                Thread(target=analyzeLogs, args=(yamlData[distribution]["postgres"],), kwargs={'key1':"Postgres"}).start()
+                Thread(target=analyzeLogs, args=(yamlData[distribution]["postgres"],),
+                       kwargs={'key1': "Postgres"}).start()
         except yaml.YAMLError as exc:
             print(exc)
 
 
 def main():
-    #We are staring our critical function in here
+    # We are staring our critical function in here
     distribution = ""
     with open("distro.yaml", 'r') as distroStream:
         try:
@@ -66,10 +73,13 @@ def main():
                 distribution = "centos"
         except yaml.YAMLError as exc:
             print(exc)
-    
+
     startMonitoring(distribution)
 
+
 if __name__ == "__main__":
-    sys.path.append('./')
-    main()
-    
+    try:
+        sys.path.append('./')
+        main()
+    except Exception as e:
+        print(e, "\n" + ' bay...')
